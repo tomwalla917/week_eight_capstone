e1b0b2f7f308e769b9b185f13020319bf7fca360
Thomas Wallace <thomas.wallace@airspaceusa.com>
2025-10-21 16:34:42 -0400

    up to dat

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,34 +11,36 @@ import Contact from './pages/Contact.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+ return (
     <Router>
       <div className="app">
-        <header className="header">
-          <p className="h1">Thomas Wallace</p>
+        <header className="header bg tertiary">
+          <div className="name">
+          <p className="h2">Thomas Wallace</p>
           <p className="h4">Full Stack Developer</p>
-        </header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          </div>
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/">Home Page</Link>
+                  <NavLink className="nav-link" to="/">Home Page</NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/portfolio">Portfolio Page</Link>
+                  <NavLink className="nav-link" to="/portfolio">Portfolio Page</NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/contact">Contact Page</Link>
+                  <NavLink className="nav-link" to="/contact">Contact Page</NavLink>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
+        </header>
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
